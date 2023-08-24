3783213313cf1a3876ee5572c3e43f936f9e4de7
alsry922 <alsry922@gmail.com>
2023-08-24 10:16:41 +0900

    first commit

---
+++ src/AppProfile.jsx
@@ -0,0 +1,31 @@
+import './App.css';
+import Profile from "./components/Profile";
+import Avatar from "./components/Avatar";
+
+function AppProfile() {
+  return <>
+    <Avatar
+      image='https://images.unsplash.com/photo-1534528741775-53994a69daeb?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1964&q=80'
+      isNew={true}
+    >
+    </Avatar>
+    <Profile
+      image='https://images.unsplash.com/photo-1534528741775-53994a69daeb?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1964&q=80'
+      name='James Kim'
+      title='프론트엔드 개발자'
+      isNew={true}
+    >
+    </Profile>
+
+    <Profile
+      image='https://images.unsplash.com/photo-1506794778202-cad84cf45f1d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=687&q=80'
+      name='Bob Yu'
+      title='프론트엔드 개발자'></Profile>
+    <Profile
+      image='https://images.unsplash.com/photo-1531746020798-e6953c6e8e04?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=764&q=80'
+      name='Anna Young'
+      title='백엔드 개발자'></Profile>
+  </>;
+}
+
+export default AppProfile;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Avatar from "./Avatar";
+
+export default function Profile({image, isNew, name, title}) {
+  return (
+    <div className='profile'>
+      <Avatar
+        image={image}
+        isNew={isNew}></Avatar>
+      <h1>{name}</h1>
+      <p>{title}</p>
+    </div>
+  );
+}
